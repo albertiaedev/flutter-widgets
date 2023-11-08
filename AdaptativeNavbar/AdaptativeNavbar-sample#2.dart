a69e29b82a03c2c9b5d520764c981893a875631b
Alberto <albertiae.digital@gmail.com>
2023-11-08 16:41:16 -0400

    Modified AdaptativeNavbar - red

--- AdaptativeNavbar/AdaptativeNavbar-sample#2.dart
+++ AdaptativeNavbar/AdaptativeNavbar-sample#2.dart
@@ -1,1 +1,17 @@
 import 'package:adaptive_navbar/adaptive_navbar.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AdaptiveNavbar(
+      items: [
+        NavBarItem(icon: Icons.home, label: 'Home'),
+        NavBarItem(icon: Icons.search, label: 'Search'),
+        NavBarItem(icon: Icons.settings, label: 'Settings'),
+      ],
+      backgroundColor: Colors.red,
+      textColor: Colors.white,
+      selectedTextColor: Colors.black,
+    );
+  }
+}
